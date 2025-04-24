9920384bb3dd5d81fadc4e8e0c3e5179aa841e15
Bruno Produit <bruno@srlabs.de>
2025-04-24 16:11:01 +0200

    fix clang issues, working shell for compiling

--- flake.nix
+++ flake.nix
@@ -9,8 +9,10 @@
   }: {
     devShells.x86_64-linux.default = let
       pkgs = import nixpkgs {system = "x86_64-linux";};
-      llvmPkgs = pkgs.llvmPackages_20;
+      llvmPkgs = pkgs.llvmPackages;
       deps = with pkgs; [
+        rustc
+        cargo
         pixman
         glib
         meson
@@ -19,7 +21,8 @@
         flex
         bison
         llvm
-        llvmPkgs.clangUseLLVM
+        llvmPkgs.clang
+        llvmPkgs.libclang
         llvmPkgs.libcxxClang
       ];
     in
@@ -27,6 +30,8 @@
         buildInputs = deps;
         shellHook = ''
           export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath deps)}";
+          export LIBCLANG_PATH="${builtins.toString llvmPkgs.libclang.lib}/lib";
+          export PATH="$PATH:${pkgs.rustc}/bin:${pkgs.cargo}/bin";
         '';
       };
   };
