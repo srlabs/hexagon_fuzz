8e27fc13df1382abf491272ac4645ff15cd3012d
Bruno Produit <bruno@srlabs.de>
2025-04-30 17:41:41 +0200

    rollback flake

--- flake.nix
+++ flake.nix
@@ -11,25 +11,11 @@
         config = {
           permittedInsecurePackages = [
             "openssl-1.1.1w"
-            "python-2.7.18.8-env"
             "python-2.7.18.8"
           ];
         };
       };
       llvmPkgs = pkgs.llvmPackages;
-      python27Packages = pkgs.python27.pkgs;
-      oldPyOpenSSL = python27Packages.pyopenssl.overridePythonAttrs (old: {
-        version = "22.0.0"; # Last version compatible with Python 2.7
-        src = pkgs.fetchPypi {
-          pname = "pyOpenSSL";
-          version = "22.0.0";
-          sha256 = "sha256-/CTTWuk7g6aCKOKyEKrSx7MMO1uOJlKLcWY6BYyDFcE=";
-        };
-      });
-
-      python27WithSsl = pkgs.python27.withPackages (ps: [
-        oldPyOpenSSL
-      ]);
       deps = with pkgs; [
         rustc
         cargo
@@ -39,7 +25,7 @@
         socat
         glib
         meson
-        python27WithSsl # Use the enhanced Python instead
+        python27Full
         ninja
         pkg-config
         flex
@@ -57,10 +43,6 @@
           export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath deps)}:${pkgs.openssl_1_1}/lib";
           export LIBCLANG_PATH="${builtins.toString llvmPkgs.libclang.lib}/lib";
           export PATH="$PATH:${pkgs.rustc}/bin:${pkgs.cargo}/bin";
-          export PYTHONPATH="$PYTHONPATH:${pkgs.python27Packages.pyopenssl}/${pkgs.python27Full.sitePackages}";
-          # Make sure Python can find OpenSSL
-          export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
-          export OPENSSL_CONF="${pkgs.openssl_1_1.out}/etc/ssl/openssl.cnf";
         '';
       };
   };
