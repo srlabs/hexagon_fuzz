a0ec595625608a591852f7102b7b49032540828c
Bruno Produit <bruno@srlabs.de>
2025-04-30 17:37:14 +0200

    openssl

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
   description = "Development shell for baseband_fuzz";
-
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-
   outputs = {
     self,
     nixpkgs,
@@ -13,11 +11,25 @@
         config = {
           permittedInsecurePackages = [
             "openssl-1.1.1w"
+            "python-2.7.18.8-env"
             "python-2.7.18.8"
           ];
         };
       };
       llvmPkgs = pkgs.llvmPackages;
+      python27Packages = pkgs.python27.pkgs;
+      oldPyOpenSSL = python27Packages.pyopenssl.overridePythonAttrs (old: {
+        version = "22.0.0"; # Last version compatible with Python 2.7
+        src = pkgs.fetchPypi {
+          pname = "pyOpenSSL";
+          version = "22.0.0";
+          sha256 = "sha256-/CTTWuk7g6aCKOKyEKrSx7MMO1uOJlKLcWY6BYyDFcE=";
+        };
+      });
+
+      python27WithSsl = pkgs.python27.withPackages (ps: [
+        oldPyOpenSSL
+      ]);
       deps = with pkgs; [
         rustc
         cargo
@@ -27,7 +39,7 @@
         socat
         glib
         meson
-        python27Full
+        python27WithSsl # Use the enhanced Python instead
         ninja
         pkg-config
         flex
@@ -42,9 +54,13 @@
       pkgs.mkShell {
         buildInputs = deps;
         shellHook = ''
-          export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath deps)}";
+          export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath deps)}:${pkgs.openssl_1_1}/lib";
           export LIBCLANG_PATH="${builtins.toString llvmPkgs.libclang.lib}/lib";
           export PATH="$PATH:${pkgs.rustc}/bin:${pkgs.cargo}/bin";
+          export PYTHONPATH="$PYTHONPATH:${pkgs.python27Packages.pyopenssl}/${pkgs.python27Full.sitePackages}";
+          # Make sure Python can find OpenSSL
+          export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+          export OPENSSL_CONF="${pkgs.openssl_1_1.out}/etc/ssl/openssl.cnf";
         '';
       };
   };
